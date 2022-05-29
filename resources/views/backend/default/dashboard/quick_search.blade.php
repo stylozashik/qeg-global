e6acdf3ccf811ccb4d3fc45063320a91f4ccd521
Ashikur Rahaman <stylozashik@gmail.com>
2022-05-29 18:40:13 +0600

    Updated School Showing Board

--- resources/views/backend/default/dashboard/quick_search.blade.php
+++ resources/views/backend/default/dashboard/quick_search.blade.php
@@ -143,10 +143,10 @@
                       </ul>
                       <div class="tab-content" id="right-tabContent">
                         <div class="tab-pane fade active show" id="right-home" role="tabpanel" aria-labelledby="right-home-tab">
-                          <p class="mb-0 m-t-30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                          <p class="mb-0 m-t-30">Changed</p>
                         </div>
                         <div class="tab-pane fade" id="right-profile" role="tabpanel" aria-labelledby="profile-right-tab">
-                          <p class="mb-0 m-t-30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                          @include('backend.default.dashboard.schools.all_school')
                         </div>
                         <div class="tab-pane fade" id="right-contact" role="tabpanel" aria-labelledby="contact-right-tab">
                           <p class="mb-0 m-t-30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
---
+++ resources/views/backend/default/dashboard/schools/all_school.blade.php
@@ -0,0 +1,116 @@
+<div class="mb-0 m-t-30">
+    <div class="row">
+
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="news-update media" style="padding: 20px;"><img class="img-fluid me-3 b-r-10" style="border-radius:60px; width:50px;" src="{{ asset('/assets/images/user.jpg') }}" alt="">
+                    <div class="media-body">
+                    <a href="#"><h6>Cheshire College South and West - Ellesmere Port</h6></a>
+                    <p style="margin-top:-12px;">Ellesmere Port, North West</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+        
+    </div>
+</div>
